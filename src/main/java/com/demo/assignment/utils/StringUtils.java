27792e4025d4200a8f9df468f5a34ce60e48dc4e
GizemAyas517 <24607637+GizemAyas517@users.noreply.github.com>
2021-03-13 19:27:20 +0300

    printed all steps to the console

--- src/main/java/com/demo/assignment/utils/StringUtils.java
+++ src/main/java/com/demo/assignment/utils/StringUtils.java
@@ -15,22 +15,32 @@ public class StringUtils {
         return str.replaceAll("([^A-Za-z0-9])+", "_");
     }
 
-    private static List<String> convertStrToLogMessage(String str){
-        List<String> stringResultList = new ArrayList<>();
+    private static List<List<String>> convertStrToLogMessage(String str){
+        ArrayList<List<String>> steps = new ArrayList<>();
         if (!str.isEmpty()){
-            String[] strlist =  str.split(" ");
-            stringResultList =  Arrays.stream(strlist).map(word -> replaceNonAlphaNumericChar(word)).collect(Collectors.toList());
-            stringResultList = stringResultList.stream().map(w -> w.toUpperCase()).collect(Collectors.toList());
+            String[] strlist =  str.split("(\\s)+");
+
+            steps.add(Arrays.stream(strlist).collect(Collectors.toList()));
+
+            List<String> stepTwo =  Arrays.stream(strlist).map(word -> replaceNonAlphaNumericChar(word)).collect(Collectors.toList());
+            steps.add(stepTwo);
+            List<String> stepThree = stepTwo.stream().map(w -> w.toUpperCase()).collect(Collectors.toList());
+            steps.add(stepThree);
         }
-        return stringResultList;
+        return steps;
     }
 
     public static String getLogMessage(String str){
-        List<String> convertedStrList = convertStrToLogMessage(str);
+        List<List<String>> steps = convertStrToLogMessage(str);
+        StringBuilder stringBuilder = new StringBuilder();
 
-        String message = convertedStrList.stream().collect(Collectors.joining(" "));
+        for (List<String> step : steps){
+            String message = step.stream().collect(Collectors.joining(" ")) + " ";
+            stringBuilder.append(message);
+        }
 
-        return message;
+
+        return stringBuilder.toString();
     }
 
 
